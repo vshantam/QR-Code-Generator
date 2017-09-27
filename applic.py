2273d655fdc22b4e714ea9f5cb113ceda86e387a
Shantam Vijayputra <vshantam@yahoo.com>
2017-09-28 01:18:24 +0530

    Update applic.py

--- applic.py
+++ applic.py
@@ -1,26 +1,19 @@
 from lib import *
-
 #setting scale_factor
-
 scale_factor=6 #or choose as you like
-
-class qrgenerator:
-	
+class qrgenerator(object):
+	@classmethod
 	def generate(self):
 		qr=pyqrcode.create(raw_input("Please Enter the requred text for creation of the QR Code:\n"),encoding='utf-8',error='L',version=27)
-		
 		#or using qrcode : qrcode.make(contents)
-		
 		#saving image
 		qr.png(os.getcwd()+str('/secret.png'),scale=scale_factor)
-
 	def openbarcode(self):
 		img=cv2.imread(os.getcwd()+str('/secret.png'),cv2.IMREAD_COLOR)
 		img=plt.imshow(img,interpolation='bicubic')
 		plt.xticks(())
 		plt.yticks(())
-		plt.show()
-			       
+		plt.show()    
 if __name__=='__main__':
 	obj=qrgenerator()
 	obj.generate()
