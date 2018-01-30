06c85608d2bbf570bf752f905adbbf8a7cdb60ea
Shantam Vijayputra <vshantam@yahoo.com>
2018-01-30 22:47:53 +0530

    Update applic.py

--- applic.py
+++ applic.py
@@ -2,10 +2,8 @@ from lib import *
 #setting scale_factor
 scale_factor=6 #or choose as you like
 class qrgenerator(object):
-	
 	@classmethod
 	def generate(self):
-		
 		qr=pyqrcode.create(raw_input("Please Enter the requred text for creation of the QR Code:\n"),encoding='utf-8',error='L',version=27)
 		#or using qrcode : qrcode.make(contents)
 		#saving image
