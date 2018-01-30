25d2701f93b718c426f8a51668f615ea140cb2a0
Shantam Vijayputra <vshantam@yahoo.com>
2018-01-30 22:34:19 +0530

    Update applic.py

--- applic.py
+++ applic.py
@@ -1,22 +1,41 @@
 from lib import *
 #setting scale_factor
+
 scale_factor=6 #or choose as you like
+
 class qrgenerator(object):
+	
 	@classmethod
 	def generate(self):
+		
 		qr=pyqrcode.create(raw_input("Please Enter the requred text for creation of the QR Code:\n"),encoding='utf-8',error='L',version=27)
 		#or using qrcode : qrcode.make(contents)
 		#saving image
-		qr.png(os.getcwd()+str('/secret.png'),scale=scale_factor)
+		result = qr.png(os.getcwd()+str('/secret.png'),scale=scale_factor)
+		
+		return result
+		
 	@classmethod
 	def openbarcode(self):
+		
 		img=cv2.imread(os.getcwd()+str('/secret.png'),cv2.IMREAD_COLOR)
 		img=plt.imshow(img,interpolation='bicubic')
 		plt.xticks(())
 		plt.yticks(())
-		plt.show()    
+		showimg = plt.show()
+		
+		return showimg
+		
+	@classmethod
+	def main(self):
+		
+		obj=qrgenerator()
+		obj.generate()
+		
+		return obj.openbarcode()
+		
 if __name__=='__main__':
-	obj=qrgenerator()
-	obj.generate()
-	obj.openbarcode()
+	
+	obj1 = qrgenerator()
+	obj1.main()
 	print("Quitting Application...")
